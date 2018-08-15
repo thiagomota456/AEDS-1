c524de2790a7fe052c3711daafc3411020abfed3
Thiago <thiagomota456@gmail.com>
2018-08-15 18:36:28 -0300

    Add função addZeroEsquerdaBigNum que não está funcionando.

--- 2.c
+++ 2.c
@@ -1,20 +1,108 @@
 #include<stdio.h>
-#define MAXREAD 15
+#define MAXREAD 200
 
+void lerBignNum(int *num);
 void printBigNum(int *num);
 void printBigNum(int *num);
-int tamanhoBigNum(int *num);
+int *menor(int *num1, int *num2);
+int casasDecimaisBigNum(int *num);
+void addZeroEsquerdaBigNum(int *num1, int *num2);
+int *maisCasasDecimaisBigNum(int *num1, int *num2);
+int *menosCasasDecimaisBigNum(int *num1, int *num2); 
+int mesmoNumDeCasasDecimais(int *num1, int *num2);
+
 
 int main(){
+	int num1[MAXREAD];
+	int num2[MAXREAD];
 
-	int num[MAXREAD];
+	printf("Digite um numero inteiro qualquer: ");
+	lerBignNum(num1);
+	
+	printf("Digite um segundo numero inteiro qualquer: ");
+	lerBignNum(num2);
 
-	printf("Digite um numero inteiro: ");
-	lerBignNum(num);
+	//Testes
+	printf("Número 1: "); printBigNum(num1); printf("\n");
+	printf("Tamnaho 1: %d\n",casasDecimaisBigNum(num1));
 
-	printBigNum(num);
-	printf("Tamnaho %d\n",tamanhoBigNum(num));
+	printf("\n");
+	
+	printf("Número 2: "); printBigNum(num2); printf("\n");
+	printf("Tamnaho 2: %d\n",casasDecimaisBigNum(num2));
+
+	printf("Mais casas decimais: ");printBigNum(maisCasasDecimaisBigNum(num1,num2)); printf("\n");
+	printf("Menos casas decimais: ");printBigNum(menosCasasDecimaisBigNum(num1,num2)); printf("\n");
+
+	addZeroEsquerdaBigNum(num1, num2);
+	printf("\n");
+
+	if(!(mesmoNumDeCasasDecimais(num1,num2)))
+		printBigNum(menosCasasDecimaisBigNum(num1,num2)); printf("\n");
+
+
+
+}//end main
+
+
+void addZeroEsquerdaBigNum(int *num1, int *num2){
+
+	int addZeros[MAXREAD];
+	int *menorNum = menor(num1, num2);
+	int numerosDeZeros = casasDecimaisBigNum(maisCasasDecimaisBigNum(num1,num2)) - casasDecimaisBigNum(menosCasasDecimaisBigNum(num1,num2));
+	int i, j;
+
+	for(i = 0; i < casasDecimaisBigNum(menosCasasDecimaisBigNum(num1, num2)); i++){
+		for(j = 0; j < numerosDeZeros; j++){
+			addZeros[j] = 0;
+		}
+
+
+		addZeros[i+j] = menorNum[i];  
+	}
+
+	if(menor(num1, num2) == num1)
+		num1 = menorNum;
+	else
+		num2 = menorNum;
+
+}//end addZeroEsquerda
+
+int *menor(int *num1, int *num2){
+	int i;
+
+	//Se os numeros tiverem mesmo número de casas decimais
+	if(mesmoNumDeCasasDecimais(num1,num2)){
+		for(i = 0; i < casasDecimaisBigNum(num1); i++){
+			if(num1[i] < num2[i])
+				return num1;
+			if(num1[i] > num2[i])
+				return num2;
+		}
+
+	}
+	else
+		return menosCasasDecimaisBigNum(num1, num2);
+		//Consíderando que o usuário não tenha satánas no coração de dígitar zero a esquerda, o menor numero será sempre o de menor número de casas decimais
+		//Caso necessário, posso garantir que não haja zeros a esquerda na leitura do número.
+		
+	//Se forem iguais vai retornar falso
+	return 0;
 }
+
+int mesmoNumDeCasasDecimais(int *num1, int *num2){
+	return ( casasDecimaisBigNum(num1) == casasDecimaisBigNum(num2) );
+}
+
+int *maisCasasDecimaisBigNum(int *num1, int *num2){
+	//Condição ? verdadeiro : falso
+	return (casasDecimaisBigNum(num1) > casasDecimaisBigNum(num2)) ? num1 : num2;
+}//end maior
+
+int *menosCasasDecimaisBigNum(int *num1, int *num2){
+	//Condição ? verdadeiro : falso
+	return (casasDecimaisBigNum(num1) < casasDecimaisBigNum(num2)) ? num1 : num2;
+}//end maior
 
 void lerBignNum(int *num){
 
@@ -30,7 +118,7 @@ void lerBignNum(int *num){
 	//Marca o final o número
 	num[i] = -1;
 
-}
+}//end lerBignNum
 
 void printBigNum(int *num){
 
@@ -39,14 +127,15 @@ void printBigNum(int *num){
 	for(i = 0; num[i] != -1; i++){
 		printf("%d",num[i]);
 	}
-	printf("\n");
-}
 
+}//end printBigNum
+
+/*
 void soma(int *resultado, int *num1, int *num2){
 	//fazer a soma acessano os elementos de tras pra frente
 	for()
 }
- 
+
 void somar_um_elemento( int *vai_um, int *resultado, int *num1, int *num2){
 	int aux = *num1 + *num2;
 
@@ -60,12 +149,13 @@ void somar_um_elemento( int *vai_um, int *resultado, int *num1, int *num2){
 	}
 
 }
-
-int tamanhoBigNum(int *num){
+*/
+int casasDecimaisBigNum(int *num){
 	int i = 0;
 
 	while(num[i++] != -1)//Compara antes de incrementar. Ao achar, incrementa mais um.
 		;
 
 	return i-1;
-}
+
+}//end CasasDecimaisBigNum
