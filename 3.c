8870f914f87714c2e65928312f5de6225b3d5742
Aluno <thiagomota456@gmail.com>
2018-08-24 12:38:02 -0300

    Add funções pra printar, somar e trabalhado na decompor

--- 3.c
+++ 3.c
@@ -1,8 +1,74 @@
 #include<stdio.h>
 
+void mostrar(int *n);
+int soma_todo_vetor(int *n);
+
+
+int num;
+
 int main(){
-int 
+
+	printf("Digite um numero inteiro: ");
+	scanf("%d", &num);
+
+	printf("Decomposicao:\n");
+	//O proprio número printado que sempre acontece
+	printf("%d\n",num );
+
+	decompor(num);
+
+
 }
 
-void 
+void decompor(int number){
 
+
+	//O máximo de posiçoes que preciso é igual ao número de  
+	int vetor[num];
+	int i;
+
+	//Zero todas as posições do vetor
+	memset(vetor, 0,  number * sizeof(vetor));
+
+	//Decremento
+	//number = number--;
+	//1° posição do vetor igual a número decrementado
+	vetor[0] = number;
+
+	//for(i = 1; i <= number; i++){
+
+		//if( soma_todo_veto(vetor) == num ) //vetor[i] = number;
+		//	mostrar(vetor);
+	//}
+
+}//end decompor
+
+//Funcionando
+int soma_todo_vetor(int *n){
+	int aux = 0;
+	int i;
+
+	for(i = 0; i < num; i++){
+		aux += n[i];
+	}
+
+	return aux;
+}//end soma_todo_vetor
+
+
+//Funcionando
+void mostrar(int *n){
+	int i;
+
+	for (i = 0; i < num; ++i){
+
+		if(n[i] != 0)//Se não é 0
+			printf("%d", n[i]);//printa o número
+
+		if(i+1 != num)
+			printf(" + ");
+		else
+			printf("\n");
+	}//end for
+
+}//end mostrar
