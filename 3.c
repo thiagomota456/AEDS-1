b1e560a31d5782a63a44b7959080f246d8b8c141
Thiago Soares Mota <thiagomota456@gmail.com>
2018-08-24 14:34:25 -0300

    Add funções para decomosição

--- 3.c
+++ 3.c
@@ -2,7 +2,9 @@
 
 void mostrar(int *n);
 int soma_todo_vetor(int *n);
-
+void decrementar_1_posicao(int *vetor);
+void decompor(int number);
+int vetor_todo_decomposto(int *vetor);
 
 int num;
 
@@ -12,6 +14,7 @@ int main(){
 	scanf("%d", &num);
 
 	printf("Decomposicao:\n");
+
 	//O proprio número printado que sempre acontece
 	printf("%d\n",num );
 
@@ -26,22 +29,80 @@ void decompor(int number){
 	//O máximo de posiçoes que preciso é igual ao número de  
 	int vetor[num];
 	int i;
+	int aux;
 
-	//Zero todas as posições do vetor
-	memset(vetor, 0,  number * sizeof(vetor));
+	//inicialmente 
+	vetor[0] = num;
+	decrementar_1_posicao(vetor);
 
-	//Decremento
-	//number = number--;
-	//1° posição do vetor igual a número decrementado
-	vetor[0] = number;
+	//While enquando todas as posições do vetor não forem 1
+	while(!vetor_todo_decomposto(vetor)){
 
-	//for(i = 1; i <= number; i++){
+		if(soma_todo_vetor(vetor) == num){
+			mostrar(vetor);
+			decrementar_1_posicao(vetor);
+		}
+		else{
 
-		//if( soma_todo_veto(vetor) == num ) //vetor[i] = number;
-		//	mostrar(vetor);
-	//}
+			//duplico
+			aux = vetor[0];
+			vetor[1] = aux;
+
+			for (i = 0; i < num; ++i)
+			{
+				//Se não for ==  a num ainda
+				if(!testa_se_igual_a_num(vetor))
+					vetor[1]--;
+				else
+					vetor[0]--;
+			}//end for i
+
+		}//end else 
+
+	}//end while 
 
 }//end decompor
+
+int vetor_todo_decomposto(int *vetor){
+	int i;
+
+	for (i = 0; i < num; ++i)
+	{
+		if(vetor[i] != 1)
+			return 0;
+	}
+
+	return 1;
+}
+
+int testa_se_igual_a_num(int *vetor){
+
+	if(soma_todo_vetor(vetor) == num){
+		mostrar(vetor);
+		decrementar_1_posicao(vetor);
+		return 1;
+	}
+	else
+		return 0;
+
+}
+
+void decrementar_1_posicao(int *vetor){
+
+	int aux;
+
+	aux = vetor[0];
+
+	//Zero todas as posições do vetor
+	memset(vetor, 0,  num * sizeof(vetor));
+
+	//Decremento 1
+	aux--;
+
+	//add a 1° posicao  1 decrementado
+	vetor[0] = aux;
+
+}
 
 //Funcionando
 int soma_todo_vetor(int *n){
@@ -62,13 +123,15 @@ void mostrar(int *n){
 
 	for (i = 0; i < num; ++i){
 
-		if(n[i] != 0)//Se não é 0
+		if(n[i] != 0){//Se não é 0
 			printf("%d", n[i]);//printa o número
 
-		if(i+1 != num)
-			printf(" + ");
-		else
-			printf("\n");
+			//add um + se não é o ulto numero ou se o proxímo não é  
+			if( (i+1 != num) && (n[i+1] != 0) )
+				printf(" + ");
+			else
+				printf("\n");
+		}
 	}//end for
 
 }//end mostrar
