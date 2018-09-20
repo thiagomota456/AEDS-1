bc5aba11056da1baff56f567ac42646a4ff9281a
Thiago Soares Mota <thiagomota456@gmail.com>
2018-09-19 22:03:02 -0300

    Trabalhando

--- 4.Distancia entre duas cidades.c
+++ 4.Distancia entre duas cidades.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<strig.h>
 
 typedef struct cidade{
 
 	char nome[21];
-	int * caminhos;
+	int numero;
 
 }CIDADE;
 
@@ -12,54 +13,124 @@ typedef struct cidade{
 
 int ** Aloca_matriz(int **matriz, int lin, int col){
 
+	// Adaptado de https://www.ime.usp.br/~pf/algoritmos/aulas/aloca.html
+
+	matriz = realloc (matriz, lin * sizeof(int *));
+
+	for(int i = 0; i < lin; i++)
+		matriz[i] = realloc (matriz[i], col * sizeof(int));
+
+	return matriz;	
 	
-}
+}//end Aloca_matriz
+
+int cidadeCadastrada(CIDADE * cidaddes,int numero_de_cidades, char * cidade_lida1){
+
+	int i;
+
+	//Percorre todas as cidades ja existentes
+
+	for(i = 0; i < numero_de_cidades; i++)
+		if(strcmp(cidaddes[i].nome,cidade_lida1) == 0)
+			return cidaddes[i].nome;
+
+	//Retorna 1
+
+	return 1;
+
+}//end cidadeCadastrada
 
 //Função que le o arquivo nos padroes especificados
 
-void lerArquivo(int *numeroDeCidades, CIDADE **matriz, char *nomeDoArquivo){	
+void * lerArquivo( char *nome_do_arquivo, char cidade_de_origem, char * cidade_de_destino, CIDADE * cidaddes, int numero_de_cidades_lidas){
 
-	FILE *PonteiroDeArquivo;
+
+	//Aprentemente eu não reciso receber número de arrestas
+	//Ver isso depois
+
+	FILE * PonteiroDeArquivo;
+
+	PonteiroDeArquivo = fopen(nome_do_arquivo, "r");
+
+	int numero_de_cidades_e_pesos;
 	int i = 0;
-	int auxiliar;
 
-	//Abri arquivo para leitura
+	fscanf("%d", &numero_de_cidades_e_pesos);
 
-	PonteiroDeArquivo = fopen(nomeDoArquivo, "r");
+	char cidade_lida1[21];
+	char cidade_lida2[21];
 
-	//Testa se realizoua a litura correta, 
-	
-	if(PonteiroDeArquivo == NULL){
+	int linha, coluna, distancia;
 
-		printf("ERRO: NAO FOI POSSIVEL ABRIR O ARQUIVO\n");
-		return;
-	}
+	//Inicio minha matriz de cidades
 
-	//Leio mochila
+	*matriz_de_distancias = malloc( 1 * sizeof(int));
 
-	fscanf( PonteiroDeArquivo, " %d", numeroDeCidades);
+	while(i < numero_de_cidades_e_pesos){
 
-	//auxiliar pra cria matriz e ler itens mais facilmente
+		//leio a linha
 
-	auxiliar = *numeroDeCidades;
+		fscanf(PonteiroDeArquivo,"%s %s %d", cidade_lida1, cidade_lida2, &distancia);
 
-	printf("%d", auxiliar);
-	//Crio matriz de cidades
+		//Caso a cidade não exita
 
-	*matriz = (CIDADE *) malloc(auxiliar * sizeof(CIDADE *));
-	for(i = 0; i < auxiliar; i++){
+		if( cidadeCadastrada(cidaddes, (*numero_de_cidades_lidas), cidade_lida1) ){
 
-		(*matriz)[i].caminhos = (int *) malloc(auxiliar * sizeof(int)); 
+			//Cadastro nova cidade
 
-	}//end for
+			strcpy(cidaddes[(*numero_de_cidades_lidas)].nome, cidade_lida1);
+
+			//Add número da cidade
+			
+			cidaddes[(*numero_de_cidades_lidas)].numero = (*numero_de_cidades_lidas);
+
+			//Atribuo linha da cidade
+
+			linha = (*numero_de_cidades_lidas);
+
+			//Incremento número de cidades
+
+			(*numero_de_cidades_lidas) = (*numero_de_cidades_lidas) + 1;
+
+					 
+			
+
+		}else{
+			//Se ja foi cadastrado add a linha da cidade que é a mesma que seu numero
+
+			//Depois criar uma função especifica so pra pesquisar cidades por nome			
+			linha = cidadeCadastrada(cidaddes, (*numero_de_cidades_lidas), cidade_lida1);
+
+		}
+
+
+		//Caso cidade não exista			
+				
+
+	}//end while(i<numero_de_cidades_e_pesos)
 
 	fclose(PonteiroDeArquivo);
+
 }//end lerArquivo
 
 int main(){
 
-	int numCidades;
-	CIDADE *matriz;
+	CIDADE cidaddes[1000];
+	int numero_de_cidades= 0;
+	int *matriz_de_distancias;
 
-	lerArquivo(&numCidades, &matriz, "file.txt");
+	/***********Criar funções que pesquisa cidade e retorna numero**********/
+	
+	char nome_do_arquivo[100];
+	char cidade_de_origem[21];
+	char cidade_de_destino[21];
+
+	//Leitura do arquivo
+
+	printf("Digite o nome do arquivo de entrada: ");
+	scanf("%s", nome_do_arquivo);
+	lerArquivo(matriz_de_distancias, nome_do_arquivo, &cidade_de_origem, &cidade_de_destino, &cidaddes, &numero_de_cidades);
+
+	
+	
 }//end main
