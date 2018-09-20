6ddc9a3c1201ada609f093ca2484192950043a59
Thiago Soares Mota <thiagomota456@gmail.com>
2018-09-20 20:20:13 -0300

    Erro ao printar

--- 4.Distancia entre duas cidades.c
+++ 4.Distancia entre duas cidades.c
@@ -2,6 +2,7 @@
 #include<stdlib.h>
 #include<string.h>
 #include <math.h>
+#define FLSH gets(l)
 
 /* definir variavies */
 int destino, origem, vertices = 0;
@@ -17,7 +18,7 @@ typedef struct cidade{
 //Pra ver se cidades e numeros foram associados corretamente
 
 void printarVetorDeCidades(CIDADE * cidaddes, int quantidade){
-	
+
 	int i;
 
 	for(i = 0; i < quantidade; i++){
@@ -39,7 +40,7 @@ void printarMatrizQuadrada(int **matriz, int tamanho){
 
 		printf("\n");
 	}//end for i
-			
+
 }//end printarMatrizQuadrada
 
 //função que aloca matriz
@@ -53,8 +54,8 @@ int ** Aloca_matriz(int **matriz, int lin, int col){
 	for(int i = 0; i < lin; i++)
 		matriz[i] = realloc (matriz[i], col * sizeof(int));
 
-	return matriz;	
-	
+	return matriz;
+
 }//end Aloca_matriz
 
 int cidadeCadastrada(CIDADE * cidaddes,int numero_de_cidades, char * cidade_lida1){
@@ -68,16 +69,16 @@ int cidadeCadastrada(CIDADE * cidaddes,int numero_de_cidades, char * cidade_lida
 		//verifica se a cidade já
 
 		if(strcmp(cidaddes[i].nome, cidade_lida1) == 0){
-			
+
 			//Caso cidade encontrada
 
 			return 1;
 		}
-
+#define FLSH gets(l)
 	}//end for
 
 	//Caso não encontrada
- 
+
 	return 0;
 
 }//end cidadeCadastrada
@@ -104,14 +105,14 @@ int NumeroDaCidadeCadastrada(CIDADE * cidaddes,int numero_de_cidades, char * cid
 	}//end for
 
 	//Caso não encontrado
- 
+
 	return -1;
 
 }//end NumeroDaCidadeCadastrada
 
 //Função que le o arquivo nos padroes especificados
 //Depois quero simplificar mais a lsita de paramentros dessa função e separala em mais funções para tornal mais legivel
-		
+
 int ** lerArquivo( char *nome_do_arquivo, char * cidade_de_origem, char * cidade_de_destino, CIDADE * cidaddes, int * numero_de_cidades_lidas){
 
 
@@ -126,11 +127,11 @@ int ** lerArquivo( char *nome_do_arquivo, char * cidade_de_origem, char * cidade
 	int i = 0;
 
 	fscanf(PonteiroDeArquivo, "%d", &numero_de_cidades_e_pesos);
- 
+
 	//variaveis pra add valores a matriz de ciades
 
 	int linha, coluna, distancia;
-	
+
 	//Inicio minha matriz de cidades
 
 	matriz_de_distancias = malloc(sizeof(int));
@@ -140,7 +141,7 @@ int ** lerArquivo( char *nome_do_arquivo, char * cidade_de_origem, char * cidade
 		//leio a linha
 
 		fscanf(PonteiroDeArquivo,"%s %s %d", cidade_de_destino, cidade_de_origem, &distancia);
- 
+
 		//Caso a cidade não exita
 
 		if( cidadeCadastrada(cidaddes, *numero_de_cidades_lidas, cidade_de_destino) == 0){
@@ -150,7 +151,7 @@ int ** lerArquivo( char *nome_do_arquivo, char * cidade_de_origem, char * cidade
 			strcpy(cidaddes[*numero_de_cidades_lidas].nome, cidade_de_destino);
 
 			//Add número da cidade
-			
+
 			cidaddes[*numero_de_cidades_lidas].numero = (*numero_de_cidades_lidas);
 
 			//Atribuo linha da cidade
@@ -164,15 +165,15 @@ int ** lerArquivo( char *nome_do_arquivo, char * cidade_de_origem, char * cidade
 			//Aloca matriz que contem as distancias das cidades
 
 			matriz_de_distancias = Aloca_matriz(matriz_de_distancias, *numero_de_cidades_lidas,*numero_de_cidades_lidas);
-			
+
 
 		}else{
 			//Se ja foi cadastrado add a linha da cidade que é a mesma que seu numero
-			
+
 			linha = NumeroDaCidadeCadastrada(cidaddes, (*numero_de_cidades_lidas), cidade_de_destino);
 
 		}//end cadastro cidae 1
- 
+
 		//Caso a cidade não exita
 
 		if( cidadeCadastrada(cidaddes, *numero_de_cidades_lidas, cidade_de_origem) == 0){
@@ -182,7 +183,7 @@ int ** lerArquivo( char *nome_do_arquivo, char * cidade_de_origem, char * cidade
 			strcpy(cidaddes[*numero_de_cidades_lidas].nome, cidade_de_origem);
 
 			//Add número da cidade
-			
+
 			cidaddes[*numero_de_cidades_lidas].numero = (*numero_de_cidades_lidas);
 
 			//Atribuo linha da cidade
@@ -199,15 +200,15 @@ int ** lerArquivo( char *nome_do_arquivo, char * cidade_de_origem, char * cidade
 
 		}else{
 			//Se ja foi cadastrado add a coluna da cidade que é a mesma que seu numero
-		
+
 			coluna = NumeroDaCidadeCadastrada(cidaddes, (*numero_de_cidades_lidas), cidade_de_origem);
 
 		}//end cadastro cidade 2
 
 		//add peso da cidae 1 pra cidade 2  e da cidade 2 pra cidade
-	
+
 		matriz_de_distancias[linha][coluna] = distancia;
-		matriz_de_distancias[coluna][linha] = distancia;		
+		matriz_de_distancias[coluna][linha] = distancia;
 
 		//Incremento
 
@@ -229,7 +230,7 @@ int ** lerArquivo( char *nome_do_arquivo, char * cidade_de_origem, char * cidade
 
 	printarVetorDeCidades( cidaddes, *numero_de_cidades_lidas);
 
-	printarMatrizQuadrada(matriz_de_distancias, *numero_de_cidades_lidas);	
+	printarMatrizQuadrada(matriz_de_distancias, *numero_de_cidades_lidas);
 
 	//Retona matriz de distancia
 
@@ -249,8 +250,8 @@ void add(int numero_de_cidades, int **matriz_de_distancias)
 	//Não sei exatamente o pq dessa porra, mas deixa por enquanto
 	//Mas acho que é pq o algoritimo que éguei podia receber mais d eum grafo por execução
 
-	if (!custos)
-		free(custos);
+//	if (!custos)
+	//	free(custos);
 
 	custos = (int *) malloc(sizeof(int)*vertices*vertices);
 
@@ -264,7 +265,7 @@ void add(int numero_de_cidades, int **matriz_de_distancias)
 		//Para todas as vertices possiveis add caminhos
 
 		for(j = 0; j < vertices; j++){
-		
+
 			//Recebo origem (indexando de um pq o algoritimmo fonte faz assim)
 
 			origem = i+1;
@@ -279,99 +280,116 @@ void add(int numero_de_cidades, int **matriz_de_distancias)
 
 			custos[(origem-1) * vertices + destino - 1] = custo;
 
-		}//end for j
-			
+		}//end for j#define FLSH gets(l)
+
 	}//end for i
 
 }//end add
 
-void dijkstra(int vertices,int origem,int destino, int *custos, CIDADE *listaDeCidades){
 
-printf("Declaração de variaveis");
+// Fonte : https://www.revista-programar.info/artigos/algoritmo-de-dijkstra/
 
-  int i,v, cont = 0;
-  int *ant, *tmp; 
-  int *z; /* vertices para os quais se conhece o caminho minimo */
-  double min;
-  double dist[vertices]; /* vetor com os custos dos caminhos */
+void dijkstra(int vertices,int origem,int destino,int *custos, CIDADE * listaDeCidades)
+{
+	printf("ENTRA PORRA 1\n" );
+   int i,v, cont = 0;
+   int *ant, *tmp;
+   int *z;     /* vertices para os quais se conhece o caminho minimo */
+   double min;
+   double dist[vertices]; /* vetor com os custos dos caminhos */
+
+	 printf("ENTRA PORRA 2\n" );
+
+   /* aloca as linhas da matriz */
+   ant = calloc (vertices, sizeof(int *));
+   tmp = calloc (vertices, sizeof(int *));
+   if (ant == NULL) {
+      printf ("** Erro: Memoria Insuficiente **");
+      exit(-1);
+   }
+
+	 printf("ENTRA PORRA 3\n" );
+
+   z = calloc (vertices, sizeof(int *));
+   if (z == NULL) {
+      printf ("** Erro: Memoria Insuficiente **");
+      exit(-1);
+   }
+
+	 printf("ENTRA PORRA 4\n" );
+
+   for (i = 0; i < vertices; i++) {
+      if (custos[(origem - 1) * vertices + i] !=- 1) {
+         ant[i] = origem - 1;
+         dist[i] = custos[(origem-1)*vertices+i];
+      }
+      else {
+         ant[i]= -1;
+         dist[i] = HUGE_VAL;
+      }
+      z[i]=0;
+   }
+   z[origem-1] = 1;
+   dist[origem-1] = 0;
+
+	 printf("ENTRA PORRA 5\n" );
+
+   /* Laco principal */
+   do {
+
+      /* Encontrando o vertice que deve entrar em z */
+      min = HUGE_VAL;
+      for (i=0;i<vertices;i++)
+         if (!z[i])
+            if (dist[i]>=0 && dist[i]<min) {
+               min=dist[i];v=i;
+            }
+
+      /* Calculando as distancias dos novos vizinhos de z */
+      if (min != HUGE_VAL && v != destino - 1) {
+         z[v] = 1;
+         for (i = 0; i < vertices; i++)
+            if (!z[i]) {
+               if (custos[v*vertices+i] != -1 && dist[v] + custos[v*vertices+i] < dist[i]) {
+                     dist[i] = dist[v] + custos[v*vertices+i];
+                  ant[i] =v;
+                  }
+              }
+      }
+   } while (v != destino - 1 && min != HUGE_VAL);
+
+	 printf("ENTRA PORRA 6\n" );
+
+	 /* Mostra o Resultado da procura */
+	 printf("ENTRA PORRA 7\n" );
+
+     i = destino;
+     i = ant[i-1];
+
+		 printf("ENTRA PORRA 8\n" );
+
+		 /***************************O Erro esta aqui ***********************************/
+     /*while (i != -1) {
+
+       tmp[cont] = i+1;
+       cont++;
+       i = ant[i];
+
+			 printf("Laçço PORRA\n" );
+
+     }*/
+
+		 printf("ENTRA PORRA 9\n" );
+     for (i = cont; i > 0 ; i--) {
+       printf("%s ", listaDeCidades[tmp[i-1]].nome);
+     }
+
+		 printf("ENTRA PORRA 10\n" );
+     printf("%s ", listaDeCidades[destino-1].nome);
+
+     printf("\nDistancia total: %d Km",(int) dist[destino-1]);
 
 
-  /* aloca as linhas da matriz */
-  ant = calloc (vertices, sizeof(int *));
-  tmp = calloc (vertices, sizeof(int *));
-
-printf("Alocação");
-
-  if (ant == NULL) {
-    printf ("** Erro: Memoria Insuficiente **");
-    exit(-1);
-  }
-
-  z = calloc (vertices, sizeof(int *));
-
-  if (z == NULL) {
-    printf ("** Erro: Memoria Insuficiente **");
-    exit(-1);
-  }
-
-  for (i = 0; i < vertices; i++) {
-    if (custos[(origem - 1) * vertices + i] !=- 1) {
-      ant[i] = origem - 1;
-      dist[i] = custos[(origem-1)*vertices+i];
-    }
-
-    else {
-      ant[i]= -1;
-      dist[i] = HUGE_VAL;
-    }
-    z[i]=0;
-  }
-  z[origem-1] = 1;
-  dist[origem-1] = 0;
-
-  /* Ciclo principal */
-  do {
-    /* Encontra o vertice que deve entrar em z */
-    min = HUGE_VAL;
-    for (i=0;i<vertices;i++)
-      if (!z[i])
-        if (dist[i]>=0 && dist[i]<min) {
-          min=dist[i];v=i;
-        }
-
-    /* Calcula distancias dos novos vizinhos de z */
-    if (min != HUGE_VAL && v != destino - 1) {
-      z[v] = 1;
-      for (i = 0; i < vertices; i++)
-        if (!z[i]) {
-          if (custos[v*vertices+i] != -1 && dist[v] + custos[v*vertices+i] < dist[i]) {
-            dist[i] = dist[v] + custos[v*vertices+i];
-            ant[i] =v;
-          }
-        }
-    }
-  } while (v != destino - 1 && min != HUGE_VAL);
-
-  /* Mostra o Resultado da procura */
-  
-    i = destino;
-    i = ant[i-1];
-    while (i != -1) {
-      
-      tmp[cont] = i+1;
-      cont++;
-      i = ant[i];
-
-    }
- 
-    for (i = cont; i > 0 ; i--) {
-      printf("%s ", listaDeCidades[tmp[i-1]].nome);
-    }
-
-    printf("%s ", listaDeCidades[destino-1].nome);
- 
-    printf("\nDistancia total: %d Km",(int) dist[destino-1]);
- 
 }//end dijkstra
 
 int main(){
@@ -381,7 +399,7 @@ int main(){
 	int **matriz_de_distancias;
 
 	/***********Criar funções que pesquisa cidade e retorna numero**********/
-	
+
 	char nome_do_arquivo[100];
 	char cidade_de_origem[21];
 	char cidade_de_destino[21];
@@ -390,11 +408,12 @@ int main(){
 
 	printf("Digite o nome do arquivo de entrada: ");
 	scanf("%s", nome_do_arquivo);
-	
+
 	matriz_de_distancias = lerArquivo(nome_do_arquivo, cidade_de_origem, cidade_de_destino, cidaddes, &numero_de_cidades);
 
 
 	//Add a estrutura que o alg. que peguei da net trabalha
+
 	add(numero_de_cidades, matriz_de_distancias);
 
 	//Passo pro alg. Achar o menor percurso
@@ -406,9 +425,6 @@ int main(){
 	printf("Destino: %d\n", destinoDeProcura);
 
 	//dijkstra(vertices, i,j, custos)
+	dijkstra( numero_de_cidades, origemDeProcura, destinoDeProcura, custos, cidaddes);
 
-	dijkstra(numero_de_cidades, origemDeProcura, destinoDeProcura, custos, cidaddes);
-
-	
 }//end main
-
