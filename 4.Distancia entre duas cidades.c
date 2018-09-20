aa9b3a99ea540c9ab49a7dd035391652b2929a1f
Thiago Soares Mota <thiagomota456@gmail.com>
2018-09-20 10:57:40 -0300

    Leiturta de arquivo aparentemente funcionando Add arquivo de testes fata fazer mais testes

--- 4.Distancia entre duas cidades.c
+++ 4.Distancia entre duas cidades.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<strig.h>
+#include<string.h>
 
 typedef struct cidade{
 
@@ -26,45 +26,66 @@ int ** Aloca_matriz(int **matriz, int lin, int col){
 
 int cidadeCadastrada(CIDADE * cidaddes,int numero_de_cidades, char * cidade_lida1){
 
+/*
+tenho que alterar essa função
+*/
+
+	printf("\n\n\n");
+
 	int i;
 
 	//Percorre todas as cidades ja existentes
 
-	for(i = 0; i < numero_de_cidades; i++)
-		if(strcmp(cidaddes[i].nome,cidade_lida1) == 0)
-			return cidaddes[i].nome;
+	for(i = 0; i < numero_de_cidades; i++){
 
-	//Retorna 1
+		printf("Cidade %s de inicide %d\n", cidaddes[i].nome, i);
 
-	return 1;
+		if(strcmp(cidaddes[i].nome, cidade_lida1) == 0){
+			printf("Encontrei a cidae\n");
+			return cidaddes[i].numero +1;
+		}
+
+
+	}
+
+	//Retorna 0
+	printf("\n\n\n");
+
+	return 0;
 
 }//end cidadeCadastrada
 
 //Função que le o arquivo nos padroes especificados
-
-void * lerArquivo( char *nome_do_arquivo, char cidade_de_origem, char * cidade_de_destino, CIDADE * cidaddes, int numero_de_cidades_lidas){
+		//(nome_do_arquivo, &cidade_de_origem, &cidade_de_destino, &cidaddes, &numero_de_cidades);
+int ** lerArquivo( char *nome_do_arquivo, char * cidade_de_origem, char * cidade_de_destino, CIDADE * cidaddes, int * numero_de_cidades_lidas){
 
 
 	//Aprentemente eu não reciso receber número de arrestas
 	//Ver isso depois
 
 	FILE * PonteiroDeArquivo;
-
+	int ** matriz_de_distancias;
 	PonteiroDeArquivo = fopen(nome_do_arquivo, "r");
+
+	printf("Declarações ok\n");
 
 	int numero_de_cidades_e_pesos;
 	int i = 0;
 
-	fscanf("%d", &numero_de_cidades_e_pesos);
+	fscanf(PonteiroDeArquivo, "%d", &numero_de_cidades_e_pesos);
+	printf("Numero de linhas %d\n", numero_de_cidades_e_pesos);
 
 	char cidade_lida1[21];
 	char cidade_lida2[21];
 
-	int linha, coluna, distancia;
+	printf("Cheguei aqui ?");
 
+	int linha, coluna, distancia;
+	
 	//Inicio minha matriz de cidades
 
-	*matriz_de_distancias = malloc( 1 * sizeof(int));
+	*matriz_de_distancias = malloc( 490 * sizeof(int));
+	printf("Alocação da matriz ok\n");
 
 	while(i < numero_de_cidades_e_pesos){
 
@@ -72,52 +93,109 @@ void * lerArquivo( char *nome_do_arquivo, char cidade_de_origem, char * cidade_d
 
 		fscanf(PonteiroDeArquivo,"%s %s %d", cidade_lida1, cidade_lida2, &distancia);
 
+		printf("Lido: %s %s %d\n", cidade_lida1, cidade_lida2, distancia);
+
+		//Cidade Lida 1
 		//Caso a cidade não exita
 
-		if( cidadeCadastrada(cidaddes, (*numero_de_cidades_lidas), cidade_lida1) ){
+		if( cidadeCadastrada(cidaddes, *numero_de_cidades_lidas, cidade_lida1) == 0){
+
+			printf("Cadastrando cidade %s\n", cidade_lida1);
 
 			//Cadastro nova cidade
 
-			strcpy(cidaddes[(*numero_de_cidades_lidas)].nome, cidade_lida1);
+			strcpy(cidaddes[*numero_de_cidades_lidas].nome, cidade_lida1);
+
+			printf("Guardei %s no meu vetor de vidade com indice %d\n",cidaddes[*numero_de_cidades_lidas].nome,  *numero_de_cidades_lidas);
 
 			//Add número da cidade
 			
-			cidaddes[(*numero_de_cidades_lidas)].numero = (*numero_de_cidades_lidas);
+			cidaddes[*numero_de_cidades_lidas].numero = (*numero_de_cidades_lidas);
 
 			//Atribuo linha da cidade
 
-			linha = (*numero_de_cidades_lidas);
+			linha = *numero_de_cidades_lidas;
 
 			//Incremento número de cidades
 
-			(*numero_de_cidades_lidas) = (*numero_de_cidades_lidas) + 1;
+			*numero_de_cidades_lidas = *numero_de_cidades_lidas + 1;
 
-					 
+			/******************Falta alocar matriz***********/
 			
 
 		}else{
 			//Se ja foi cadastrado add a linha da cidade que é a mesma que seu numero
 
 			//Depois criar uma função especifica so pra pesquisar cidades por nome			
-			linha = cidadeCadastrada(cidaddes, (*numero_de_cidades_lidas), cidade_lida1);
+			linha = cidadeCadastrada(cidaddes, (*numero_de_cidades_lidas), cidade_lida1) - 1;
 
-		}
+		}//end cadastro cidae 1
 
 
-		//Caso cidade não exista			
+
+		//Cidade Lida 2
+		//Caso a cidade não exita
+
+		if( cidadeCadastrada(cidaddes, *numero_de_cidades_lidas, cidade_lida2) == 0){
+
+			printf("Cadastrando cidade %s\n", cidade_lida2);
+
+			//Cadastro nova cidade
+
+			strcpy(cidaddes[*numero_de_cidades_lidas].nome, cidade_lida2);
+
+			printf("Guardei %s no meu vetor de vidade com indice %d\n",cidaddes[*numero_de_cidades_lidas].nome,  *numero_de_cidades_lidas);
+
+			//Add número da cidade
+			
+			cidaddes[*numero_de_cidades_lidas].numero = (*numero_de_cidades_lidas);
+
+			//Atribuo linha da cidade
+
+			coluna = *numero_de_cidades_lidas;
+
+			//Incremento número de cidades
+
+			*numero_de_cidades_lidas = *numero_de_cidades_lidas + 1;
+
+			/******************Falta alocar matriz***********/
+
+		}else{
+			//Se ja foi cadastrado add a linha da cidade que é a mesma que seu numero
+
+			//Depois criar uma função especifica so pra pesquisar cidades por nome			
+			coluna = cidadeCadastrada(cidaddes, (*numero_de_cidades_lidas), cidade_lida2) - 1;
+
+		}//end cadastro cidade 2
+
+		//add peso da cidae 1 pra cidade 2  e da cidade 2 pra cidade 1
+
+					
 				
+
+		//Incremento
+		i++;
 
 	}//end while(i<numero_de_cidades_e_pesos)
 
+
+	printarVetorDeCidades(CIDADE * cidaddes, int quantidade);
+
 	fclose(PonteiroDeArquivo);
 
+	return matriz_de_distancias;
+
 }//end lerArquivo
+
+printarVetorDeCidades(){
+
+}//end 
 
 int main(){
 
 	CIDADE cidaddes[1000];
 	int numero_de_cidades= 0;
-	int *matriz_de_distancias;
+	int **matriz_de_distancias;
 
 	/***********Criar funções que pesquisa cidade e retorna numero**********/
 	
@@ -129,7 +207,10 @@ int main(){
 
 	printf("Digite o nome do arquivo de entrada: ");
 	scanf("%s", nome_do_arquivo);
-	lerArquivo(matriz_de_distancias, nome_do_arquivo, &cidade_de_origem, &cidade_de_destino, &cidaddes, &numero_de_cidades);
+	printf("%s\n", nome_do_arquivo);
+	matriz_de_distancias = lerArquivo(nome_do_arquivo, cidade_de_origem, cidade_de_destino, cidaddes, &numero_de_cidades);
+
+	
 
 	
 	
